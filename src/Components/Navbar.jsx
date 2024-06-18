a3f53258a106391b5d7905debee454de88b9efc6
Akash-goswami <ankitgirigoswami8765@gmail.com>
2024-06-18 19:31:57 +0530

    add some

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -124,7 +124,7 @@ export default function Navbar() {
           </div>
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
            <li className="text-white text-[25px]">
-            <a href="#Discription">Disription</a>
+            <a href="#Discription">Project Description</a>
            </li>
           </ul>
         </div>
